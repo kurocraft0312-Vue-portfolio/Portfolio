438cc2ed966d30230fbdd4095a8ecb148492eba6
kurocraft0312 <kurocraft0312@gmail.com>
2021-01-31 13:33:25 +0900

    add:一旦コミット（Vuetifyを捨てるため）

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,31 @@
 <template>
-  <div>
+  <div class="mt-2">
+    <v-container
+      class="layout_center"
+    >
+      <h1 class="page_title">夜鼠</h1>
+      <p class="page_title__ruby">-ヨネズミ-</p>
+      <p class="page_descri">「夜」をテーマとしたホームページ/ツール開発を行っております。</p>
 
+      <v-card
+        elevation="1"
+        class="ma-2"
+      >
+        <v-list-item
+        >
+          <v-img></v-img>
+        </v-list-item>
+      </v-card>
+
+      <v-btn
+        elevation="1"
+        href="/contact"
+      >
+        <v-icon>mdi-email</v-icon>
+        お問い合わせ
+      </v-btn>
+
+    </v-container>
   </div>
 </template>
 
@@ -11,5 +36,10 @@ export default {
 </script>
 
 <style>
-
+.layout_center {
+  text-align: center;
+}
+.page_title__ruby {
+  font-size: .8rem;
+}
 </style>
