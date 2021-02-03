0aee2951e507630444d6fe6f9b2970aeda94d15f
kurocraft0312 <kurocraft0312@gmail.com>
2021-02-03 09:54:27 +0900

    add:条件式で要素を再利用

--- pages/career.vue
+++ pages/career.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <div class="container">
-      <md-card class="card-sec">
+      <md-card class="card-sec" v-for="carrercard in carrercards" :key="carrercard.key">
         <md-card-header class="card-sec__frame">
           <md-card-header-text>
             <section class="card-frame">
-              <h2 class="md-title career-title">コーポレートサイトの構築・作成</h2>
-              <time class="md-times">2020/01~2021/03</time>
-              <p class="md-descri">会社が企業活動を宣伝するために必要な</p>
+              <h2 class="md-title career-title">{{carrercard.title}}</h2>
+              <time class="md-times">{{carrercard.time}}</time>
+              <p class="md-descri">{{carrercard.desc}}</p>
             </section>
             <div class="tag-lists">
-              <span class="tag-skill">PHP</span>
+              <span class="tag-skill" v-for="taglist in taglists" :key="taglist.key">{{taglist.skill}}</span>
             </div>
           </md-card-header-text>
         </md-card-header>
@@ -36,12 +36,40 @@
   color: #2F3F4F;
   font-size: 20%;
   font-weight: bold;
+  margin: 0 4px;
   padding: 4px;
 }
 </style>
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
-
+  data() {
+    return {
+      carrercards: [
+        {
+          title: 'コーポレートサイトの構築・作成',
+          time: '2020/01~2021/03',
+          desc: '会社が企業活動を宣伝するために必要な'
+        }
+      ],
+      taglists: [
+        {
+          skill: 'PHP'
+        },
+        {
+          skill: 'HTML5'
+        },
+        {
+          skill: 'CSS3'
+        },
+        {
+          skill: 'jQuery'
+        },
+        {
+          skill: 'MySQL'
+        }
+      ]
+    }
+  }
 })
 </script>
