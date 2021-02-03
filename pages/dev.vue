548145cf2941bb7852a2960991a7d724e90e15b6
kurocraft0312 <kurocraft0312@gmail.com>
2021-02-04 00:54:14 +0900

    add:タブ部分だけ実装

--- pages/dev.vue
+++ pages/dev.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
+    <div class="container">
+      <md-tabs md-alignment="fixed">
+        <md-tab md-label="すべて"></md-tab>
+        <md-tab md-label="HP"></md-tab>
+        <md-tab md-label="ツール"></md-tab>
+      </md-tabs>
+      <md-card class="card-sec">
+        <md-card-header class="card-sec__frame">
+          <md-card-header-text>
 
+          </md-card-header-text>
+        </md-card-header>
+      </md-card>
+    </div>
   </div>
 </template>
 <style scoped>
