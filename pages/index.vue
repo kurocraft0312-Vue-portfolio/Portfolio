78a421a3d5688b656f6d456c0af146c006d4a1cc
kurocraft0312 <kurocraft0312@gmail.com>
2021-02-05 01:10:31 +0900

    refactor:お問い合わせボタンをコンポーネント化

--- pages/index.vue
+++ pages/index.vue
@@ -18,17 +18,19 @@
           </md-card-header-text>
         </md-card-header>
       </md-card>
-      <md-button class="md-raised md-large__btn neon-btn">
-        <a href="/contact" class="md-raised__link"><md-icon class="md-raised__icon">email</md-icon>お問い合わせ</a>
-      </md-button>
+      <Contactbtn />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import Contactbtn from '../components/Contactbtn' // エラー原因を探る
 export default Vue.extend({
   name: 'Index',
+  components: {
+    Contactbtn
+  },
   data() {
     return {
       cards: [
@@ -97,28 +99,5 @@ export default Vue.extend({
 .card_section__link {
   color: #ffffff!important;
   text-decoration: none;
-}
-.md-large__btn {
-  height: 100%;
-}
-.neon-btn {
-  background-color: #303030!important;
-}
-.neon-btn:hover {
-  background-color: #e62f8b!important;
-  box-shadow: 0 0 10px #e62f8b, 0 0 40px #e62f8b, 0 0 80px #e62f8b;
-  color: #ffffff;
-}
-.md-raised__link {
-  color: #ffffff!important;
-  display: block;
-  padding: 16px;
-}
-.md-raised__link:hover {
-  text-decoration: none;
-}
-.md-raised__icon {
-  color: #ffffff!important;
-  margin-right: 8px;
 }
 </style>
