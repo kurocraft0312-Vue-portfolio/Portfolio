64d5979d1ffec8bff67ed1f1ba668851a9db357b
kurocraft0312 <kurocraft0312@gmail.com>
2021-02-05 00:50:08 +0900

    add:HTML部分を作成

--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,37 @@
 <template>
   <div>
-
+    <div class="container">
+      <md-card class="card-sec">
+        <md-card-header>
+          <md-card-header-text>
+            <div class="md-title">今取り組んでいること</div>
+            <ul class="dev-lists">
+              <li class="dev-list">CMS開発</li>
+              <li class="dev-list">Chrome拡張機能開発</li>
+              <li class="dev-list">ソースコードエディタの開発</li>
+              <li class="dev-list">Reddit/Twitter用クライアント・ツールの開発</li>
+            </ul>
+          </md-card-header-text>
+        </md-card-header>
+      </md-card>
+    </div>
   </div>
 </template>
 <style scoped>
-
+.card-sec {
+  background-color: #303030;
+  border-radius: 4px;
+  word-break: break-all;
+}
+.md-title {
+  color: #ffffff;
+  font-weight: bold;
+}
+.dev-lists {
+  color: #ffffff;
+  margin: 16px 0;
+  list-style: none;
+}
 </style>
 <script lang="ts">
 import Vue from 'vue'
