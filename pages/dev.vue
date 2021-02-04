e80ef0fadc5a16e57ae34a3af233f91a69361c00
kurocraft0312 <kurocraft0312@gmail.com>
2021-02-05 00:19:26 +0900

    add:見た目を簡易的に作成

--- pages/dev.vue
+++ pages/dev.vue
@@ -8,19 +8,31 @@
       </md-tabs>
       <md-card class="card-sec">
         <md-card-media>
-          <img src="ここに画像のパス" alt="画像の説明">
+          <img src="https://placehold.jp/600x339.png" alt="画像の説明">
         </md-card-media>
         <md-card-header class="card-sec__frame">
           <md-card-header-text>
-
+            <div class="md-title">Portfolio</div>
           </md-card-header-text>
+          <md-card-content class="md-card__con">
+            <p class="md-desri">夜鼠の個人活動をまとめた活動記録用サイトです。</p>
+          </md-card-content>
         </md-card-header>
       </md-card>
     </div>
   </div>
 </template>
 <style scoped>
-
+.card-sec__frame {
+  display: block;
+}
+.md-card__con {
+  margin: 16px 0;
+  padding: 0;
+}
+.md-desri {
+  color: #000000;
+}
 </style>
 <script lang="ts">
 import Vue from 'vue'
